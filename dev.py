ee8660d4ff618871c2330932f86a23f83a38f094
Raspi5 <raspi5@raspi>
2021-11-28 08:46:17 +0000

    0.2

--- dev.py
+++ dev.py
@@ -2,53 +2,72 @@ import curses
 from curses.textpad import Textbox, rectangle
 
 
+def draw(scrn):
 
-def draw_menu(stdscr):
 
-    stdscr.clear()
-    stdscr.refresh()
+
+    scrn.clear()
+    scrn.refresh()
+
+    topic = "-"
     
     # Alustus ikkunan edelliselle koolle
 
-    prevmaxy = stdscr.getmaxyx()[0]
-    prevmaxx = stdscr.getmaxyx()[1]
+    prevmaxy = scrn.getmaxyx()[0]
+    prevmaxx = scrn.getmaxyx()[1]
 
     while True:
+        
 
-        stdscr.refresh()
+        scrn.refresh()
+
+
         
     # Kehyksen kulmat
 
-        maxY = int(stdscr.getmaxyx()[0] - 1)
-        maxX = int(stdscr.getmaxyx()[1] - 2)
-        minY = int(stdscr.getbegyx()[0])
-        minX = int(stdscr.getbegyx()[1])
-
-    # Piirretään kehys ja estetään mahdollinen virhe out of boundarystä
-        try:
-           rectangle(stdscr, minY, minX, maxY, maxX)
-
-        except Exception as e:
-            pass
-    
-    # Piiretään hienommat kulmat
-
-        stdscr.addstr(minY, minX, "+")
-        stdscr.addstr(minY, maxX, "+")
-        stdscr.addstr(maxY, minY, "+")
-        stdscr.addstr(maxY, maxX, "+")
-
-    # Otsikko
-
-        topic = "iiiiiiiiii"
+        maxY = int(scrn.getmaxyx()[0] - 1)
+        maxX = int(scrn.getmaxyx()[1] - 2)
+        minY = int(scrn.getbegyx()[0]    )
+        minX = int(scrn.getbegyx()[1] + 1)
 
     # Haetaan ikkunan keskipiste. Jos ei ole jaollinen kahdella, niin vähennetään 0.5
 
         topicCenter = maxX / 2
 
-        if (topicCenter % 2) != 0:
+        if (maxX % 2) != 0:
             topicCenter -= 0.5
 
+    # Piirretään kehys ja estetään mahdollinen virhe out of boundarystä
+        try:
+           rectangle(scrn, minY, minX, maxY, maxX)
+           rectangle(scrn, minY + 2, minX + 1, maxY - 4, int(topicCenter))
+           rectangle(scrn, minY + 2, int(topicCenter) + 1, maxY - 4, maxX - 1)
+           rectangle(scrn, maxY - 4, minX + 1, maxY, maxX - 1)
+    
+    # Piiretään hienommat kulmat
+
+           scrn.addstr(minY, minX, "+")
+           scrn.addstr(minY, maxX, "+")
+           scrn.addstr(maxY, minX, "+")
+           scrn.addstr(maxY, maxX, "+")
+
+        except Exception as e:
+           pass
+
+    # Otsikko
+
+        
+        curses.echo()
+        scrn.addstr(maxY - 2, minX + 3, "# ")
+        
+        entry = scrn.getstr(maxY - 2, 6)
+
+        if len(entry) > 0:
+            topic = entry
+            scrn.clear()
+        
+
+            
     # Haetaan Otsikon keskipiste offsettiä varten
         
         if ((len(topic) / 2) % 2) != 0:
@@ -59,24 +78,27 @@ def draw_menu(stdscr):
 
             offset = len(topic) / 2
         
-        stdscr.addstr(1, int(topicCenter - offset), topic)
+        scrn.addstr(1, int(topicCenter - offset), topic)
+        
+
+        
 
 
         # Katsotaan onko ikkunan koko muuttunut. Jos koko muuttunut, niin clear ja refresh
 
-        if prevmaxx != stdscr.getmaxyx()[0] or prevmaxy != stdscr.getmaxyx()[1]:
-            curses.resizeterm(*stdscr.getmaxyx())
-            stdscr.clear()
-            stdscr.refresh()
+        if prevmaxx != scrn.getmaxyx()[0] or prevmaxy != scrn.getmaxyx()[1]:
+            curses.resizeterm(*scrn.getmaxyx())
+            scrn.clear()
+            scrn.refresh()
         
         # Ikkunan edellinen koko
 
-        prevmaxx = stdscr.getmaxyx()[0]
-        prevmaxy = stdscr.getmaxyx()[1]
+        prevmaxx = scrn.getmaxyx()[0]
+        prevmaxy = scrn.getmaxyx()[1]
 
 
 def main():
-    curses.wrapper(draw_menu)
+    curses.wrapper(draw)
 
 if __name__ == "__main__":
     main()
